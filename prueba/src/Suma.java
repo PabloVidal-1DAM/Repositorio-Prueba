eb0ca96ad6789bfe582fe4cb2cc9b199691c2ad6
pabvidort <pabvidort@Lliurex-TeknoAIO>
2024-03-06 19:39:05 +0100

    hello

---
+++ prueba/src/Suma.java
@@ -0,0 +1,13 @@
+public class Suma {
+
+	     int num1 = 10;
+
+	     int num2 = 10;
+	     
+	     int num3 = 30;
+
+	   public int sumar() {
+	       int resultado = num1 + num2 + num3;
+	       return resultado;
+	   }
+	}
